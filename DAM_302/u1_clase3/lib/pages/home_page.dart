cf4db7d9b35a2779bc332c10c035330ea801e6a7
carlosalten <carlos.alten@usm.cl>
2023-08-07 12:13:03 -0400

    Clase 3 StatefulWidget

---
+++ DAM_302/u1_clase3/lib/pages/home_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController emailCtrl = TextEditingController();
+  TextEditingController edadCtrl = TextEditingController();
+  String resultados = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //APPBAR
+      appBar: AppBar(
+        title: Text('Ejemplo Clase 3'),
+        backgroundColor: Colors.blue,
+      ),
+
+      //BODY
+      body: Form(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              //campo email
+              Container(
+                child: TextFormField(
+                  controller: emailCtrl,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Indique su correo electrónico',
+                  ),
+                ),
+              ),
+              //campo edad
+              Container(
+                child: TextFormField(
+                  controller: edadCtrl,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Edad',
+                  ),
+                ),
+              ),
+              //boton
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(top: 10),
+                child: FilledButton(
+                  child: Text('Enviar Datos'),
+                  onPressed: () {
+                    //capturar datos del form
+                    String email = emailCtrl.text;
+                    int edad = int.tryParse(edadCtrl.text) ?? 0;
+                    // print('EMAIL:' + email + ' EDAD:' + edad.toString());
+                    print('EMAIL:$email EDAD:$edad');
+                    setState(() {
+                      resultados = 'EMAIL:$email EDAD:$edad';
+                    });
+                  },
+                ),
+              ),
+              //resultados
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Text(resultados),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
